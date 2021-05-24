1d2fd19801192ff2c9b64d39761582fbe4482700
Yilin <xuyilin0121@gmail.com>
2021-05-24 21:41:57 +1000

    bash script of creating views

---
+++ ansible/create-views.sh
@@ -0,0 +1,17 @@
+IP=$(head -n 1 /home/ubuntu/comp90024/ansible/IP.txt)
+
+curl -X PUT http://admin:group45@$IP:5984/tweets
+
+curl -X PUT http://admin:group45@$IP:5984/tweets/_design/my_views -d '{
+  "views": {
+    "AvgSentimentsBySA2Code": {
+      "map": "function (doc) {emit(doc.SA2_code, doc.sentiment);}",
+	  "reduce": "function (keys, values){return sum(values)/values.length;}"
+    },
+    "AvgSentimentsBySA2Name": {
+	  "map": "function (doc) {emit(doc.SA2_name, doc.sentiment);}",
+	  "reduce": "function (keys, values){return sum(values)/values.length;}"
+    }
+  },
+  "language": "javascript"
+}'
